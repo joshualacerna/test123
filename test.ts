465995fad5e2937d305293500af2007585e5b89d
joshualacerna <79036446+joshualacerna@users.noreply.github.com>
2022-06-29 23:23:11 +0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -4,28 +4,29 @@ import {BuildSpecContent} from "./buildspec-content";
 export const PipelineConfig: IPipelineConfigProps = {
     serviceName: 'driver-service',
     sourceStage: {
-        repositoryName: 'driver-service',
+        repositoryName: 'driver-service'
     },
     buildStage: {
         ecrRepositoryName: 'driver-service',
-        buildSpec: BuildSpecContent,
+        buildSpec: BuildSpecContent
     },
     deployStage: {
         dev: {
             clusterName: 'DriverServiceDev',
             vpcId: 'vpc-b9dbb0c4',
             securityGroup: '',
+            
         },
         prod: {
             clusterName: 'DriverServiceProd',
             vpcId: 'vpc-b9dbb0c4',
-            securityGroup: 'sg-02537d092ba986307',
+            securityGroup: 'sg-02537d092ba986307'
 
         },
     },
     approvalStage: {
         notifyEmails: ['shashimald@gmail.com'],
-        notifyTopic: 'arn:aws:sns:us-east-1:000:driver-service-approval-notification',
+        notifyTopic: 'arn:aws:sns:us-east-1:000:driver-service-approval-notification'
     },
     notification: {
         slack: [
@@ -33,8 +34,8 @@ export const PipelineConfig: IPipelineConfigProps = {
                 channelName: 'driver-service-slack',
                 channelId: 'C02EBB23W7N',
                 workspaceId: 'T02CQ1XC99Q',
-                arn: 'arn:aws:chatbot::0000:chat-configuration/slack-channel/driver-service-slack',
+                arn: 'arn:aws:chatbot::0000:chat-configuration/slack-channel/driver-service-slack'
             }
         ]
     }
-}
+};
