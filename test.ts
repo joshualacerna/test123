57f03617b15fc9bb15480bf1c9c01b815d2f4fa0
joshualacerna <79036446+joshualacerna@users.noreply.github.com>
2022-06-29 23:20:55 +0400

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,40 @@
+import {IPipelineConfigProps} from "../lib/PipelineConfigProps";
+import {BuildSpecContent} from "./buildspec-content";
+
+export const PipelineConfig: IPipelineConfigProps = {
+    serviceName: 'driver-service',
+    sourceStage: {
+        repositoryName: 'driver-service',
+    },
+    buildStage: {
+        ecrRepositoryName: 'driver-service',
+        buildSpec: BuildSpecContent,
+    },
+    deployStage: {
+        dev: {
+            clusterName: 'DriverServiceDev',
+            vpcId: 'vpc-b9dbb0c4',
+            securityGroup: '',
+        },
+        prod: {
+            clusterName: 'DriverServiceProd',
+            vpcId: 'vpc-b9dbb0c4',
+            securityGroup: 'sg-02537d092ba986307',
+
+        },
+    },
+    approvalStage: {
+        notifyEmails: ['shashimald@gmail.com'],
+        notifyTopic: 'arn:aws:sns:us-east-1:000:driver-service-approval-notification',
+    },
+    notification: {
+        slack: [
+            {
+                channelName: 'driver-service-slack',
+                channelId: 'C02EBB23W7N',
+                workspaceId: 'T02CQ1XC99Q',
+                arn: 'arn:aws:chatbot::0000:chat-configuration/slack-channel/driver-service-slack',
+            }
+        ]
+    }
+}
